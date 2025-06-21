3f8dcf2d525890e21adba3fda079ad7a692e0b27
Bakhtyar <bkhan026@uOttawa.ca>
2025-06-20 20:53:36 -0400

    feat(backend): Configure Cloudinary for media uploads

--- backend/my-strapi-project/config/plugins.ts
+++ backend/my-strapi-project/config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+// backend/my-strapi-project/config/plugins.ts
+
+export default ({ env }) => ({
+  // ... any other plugin configurations
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
